224707c162e88e1b569249f715ce0a92930e1752
Long-Zixuan <778275146@qq.com>
2025-02-16 05:03:26 +0800

    CPU检测改为oshi库   LZX-git-2025-02-16-003

--- common/src/api/java/net/caffeinemc/mods/sodium/api/system/SystemAndGLInfo.java
+++ common/src/api/java/net/caffeinemc/mods/sodium/api/system/SystemAndGLInfo.java
@@ -30,63 +30,7 @@ public class SystemAndGLInfo
 
     public String getCPUInfo()
     {
-        String CPUInfo = "Unknown";
-        String OSInfo = System.getProperty("os.name")+" "+System.getProperty("os.version");
-
-        String os = System.getProperty("os.name").toLowerCase();
         try
-        {
-            // 使用 Runtime 类的 exec 方法执行系统命令
-            String command;
-            if (os.contains("win"))
-            {
-                command = "wmic cpu get name";
-            }
-            else if (os.contains("mac"))
-            {
-                command = "sysctl -n machdep.cpu.brand_string";
-            }
-            else if (os.contains("nix") || os.contains("nux") || os.indexOf("aix") > 0)
-            {
-                command = "lscpu";
-                if(os.contains("andr") || os.contains("harm"))//安卓也是Linux
-                {
-                    command = "cat /proc/cpuinfo";
-                }
-            }
-            else if(os.contains("andr") || os.contains("harm"))
-            {
-                command = "cat /proc/cpuinfo";
-            }
-            else
-            {
-                command = "unknown";
-            }
-
-            // 执行命令并获取输出
-            Process process = Runtime.getRuntime().exec(command);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line;
-            List<String> result = new ArrayList<String>();
-            while ((line = reader.readLine()) != null)
-            {
-                result.add(line.trim());
-            }
-            System.out.println("CPU信息："+ result.get(2));
-            CPUInfo = result.get(2);
-            reader.close();
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-            return System.getProperty("os.arch") + " based CPU";
-        }
-        if(!os.contains("win"))
-        {
-            CPUInfo = System.getProperty("os.arch") + " based CPU";
-        }
-        return CPUInfo;
-        /*try
         {
             String CPUName = "";
             // 创建 SystemInfo 实例
@@ -113,7 +57,7 @@ public class SystemAndGLInfo
         {
             e.printStackTrace();
             return System.getProperty("os.arch") + " based CPU";
-        }*/
+        }
     }
 
     public String getGLVersion()
