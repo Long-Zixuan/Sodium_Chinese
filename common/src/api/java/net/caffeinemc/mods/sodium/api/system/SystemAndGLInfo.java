ebaeabed40af1af397ee1c0e5961d1bda3a3dad3
Long-Zixuan <778275146@qq.com>
2025-02-17 12:48:19 +0800

    添加了联网获取socmap的功能   LZX-2025-02-17-005

--- common/src/api/java/net/caffeinemc/mods/sodium/api/system/SystemAndGLInfo.java
+++ common/src/api/java/net/caffeinemc/mods/sodium/api/system/SystemAndGLInfo.java
@@ -23,8 +23,18 @@ import java.util.HashMap;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.InputStream;
+import java.io.ByteArrayInputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import java.util.concurrent.*;
 
 public class SystemAndGLInfo
 {
@@ -37,27 +47,104 @@ public class SystemAndGLInfo
 
     private Map<String,String> mobileSocPathNumberToSocNameMap = new HashMap();
 
+    public String doGet(String httpurl)
+    {
+        HttpURLConnection connection = null;
+        InputStream is = null;
+        BufferedReader br = null;
+        String result = null;// 返回结果字符串
+        try {
+            // 创建远程url连接对象
+            URL url = new URL(httpurl);
+            // 通过远程url连接对象打开一个连接，强转成httpURLConnection类
+            connection = (HttpURLConnection) url.openConnection();
+            // 设置连接方式：get
+            connection.setRequestMethod("GET");
+            // 设置连接主机服务器的超时时间：15000毫秒
+            connection.setConnectTimeout(3000);
+            // 设置读取远程返回的数据时间：60000毫秒
+            connection.setReadTimeout(6000);
+            // 发送请求
+            connection.connect();
+            // 通过connection连接，获取输入流
+            if (connection.getResponseCode() == 200) {
+                is = connection.getInputStream();
+                // 封装输入流is，并指定字符集
+                br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+                // 存放数据
+                StringBuffer sbf = new StringBuffer();
+                String temp = null;
+                while ((temp = br.readLine()) != null) {
+                    sbf.append(temp);
+                    sbf.append("\r\n");
+                }
+                result = sbf.toString();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭资源
+            if (null != br) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (null != is) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            connection.disconnect();// 关闭远程连接
+        }
+
+        return result;
+    }
+
     void initmobileSocPathNumberToSocNameMap()
     {
         String jsonFilePath = "/assets/sodium/soc_map/MobileSocPathNumberToName.json";
 
         ObjectMapper objectMapper = new ObjectMapper();
-
         try (InputStream inputStream = this.getClass().getResourceAsStream(jsonFilePath)) {
             if (inputStream == null) {
                 System.err.println("JSON 文件未找到: " + jsonFilePath);
                 return;
             }
 
+            // 使用 Jackson 将 JSON 流直接转换为 Map 对象
             mobileSocPathNumberToSocNameMap = objectMapper.readValue(inputStream, Map.class);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+        String jsonStr = doGet("https://raw.githubusercontent.com/Long-Zixuan/Long-Zixuan.github.io/master/json/sodium/soc_map/MobileSocPathNumberToName.json");
+        if(jsonStr != null)
+        {
+            try
+            {
+                objectMapper = new ObjectMapper();
+                InputStream inStream = new ByteArrayInputStream(jsonStr.getBytes());
+                mobileSocPathNumberToSocNameMap = objectMapper.readValue(inStream, Map.class);
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
     private SystemAndGLInfo()
     {
-        initmobileSocPathNumberToSocNameMap();
+        CompletableFuture.runAsync(() -> {
+            initmobileSocPathNumberToSocNameMap();
+        });
     }
 
     private String getMobileSocNameWithPathNumber(String pathNumber)
